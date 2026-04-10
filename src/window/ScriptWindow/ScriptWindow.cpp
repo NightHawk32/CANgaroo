229bf00ac55d42284ff74fa3ea1ed30af7bdb34d
Patrick Felixberger <robomaniac8@googlemail.com>
2026-04-11 00:27:50 +0200

    Added copyright header

--- src/window/ScriptWindow/ScriptWindow.cpp
+++ src/window/ScriptWindow/ScriptWindow.cpp
@@ -1,3 +1,21 @@
+/*
+  Copyright (c) 2026 Schildkroet
+
+  This file is part of CANgaroo.
+
+  cangaroo is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  cangaroo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with cangaroo.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "ScriptWindow.h"
 
 #include <QVBoxLayout>
@@ -16,6 +34,7 @@
 #include <core/PythonEngine.h>
 #include <core/Backend.h>
 #include <core/CanTrace.h>
+
 
 ScriptWindow::ScriptWindow(QWidget *parent, Backend &backend)
     : ConfigurableWidget(parent)
@@ -36,7 +55,7 @@ ScriptWindow::ScriptWindow(QWidget *parent, Backend &backend)
     _btnSave  = new QPushButton(tr("Save"));
 
     _chkAutoRun = new QCheckBox(tr("AutoRun"));
-    _chkAutoRun->setToolTip(tr("Start/Stop script with measurement"));
+    _chkAutoRun->setToolTip(tr("Start script with measurement"));
     _btnStop->setEnabled(false);
 
     toolbar->addWidget(_btnRun);
@@ -119,7 +138,7 @@ void ScriptWindow::retranslateUi()
     _btnLoad->setText(tr("Load"));
     _btnSave->setText(tr("Save"));
     _chkAutoRun->setText(tr("AutoRun"));
-    _chkAutoRun->setToolTip(tr("Start/Stop script with measurement"));
+    _chkAutoRun->setToolTip(tr("Start script with measurement"));
 }
 
 void ScriptWindow::onRunClicked()
@@ -234,7 +253,7 @@ void ScriptWindow::onMeasurementStarted()
 
 void ScriptWindow::onMeasurementStopped()
 {
-    if (_chkAutoRun->isChecked() && _engine->isRunning())
+    if (_engine->isRunning())
     {
         _engine->stopScript();
     }
